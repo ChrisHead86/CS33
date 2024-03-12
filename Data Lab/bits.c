cb9bfc0f12033854c616810ecfbc242a50487878
ChrisHead86 <125095208+ChrisHead86@users.noreply.github.com>
2024-03-12 13:40:42 -0700

    Create bits.c

---
+++ Data Lab/bits.c
@@ -0,0 +1,314 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Christopher Head 005611217>
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting if the shift amount
+     is less than 0 or greater than 31.
+
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+
+NOTES:
+  1. Our checker requires that you do NOT define a variable after 
+     a statement that does not define a variable.
+
+     For example, this is NOT allowed:
+
+     int illegal_function_for_this_lab(int x, int y) {
+      // this statement doesn't define a variable
+      x = x + y + 1;
+      
+      // The checker for this lab does NOT allow the following statement,
+      // because this variable definition comes after a statement 
+      // that doesn't define a variable
+      int z;
+
+      return 0;
+     }
+     
+  2. VERY IMPORTANT: Use the dlc (data lab checker) compiler (described in the handout)
+     to check the legality of your solutions.
+  3. Each function has a maximum number of operations (integer, logical,
+     or comparison) that you are allowed to use for your implementation
+     of the function.  The max operator count is checked by dlc.
+     Note that assignment ('=') is not counted; you may use as many of
+     these as you want without penalty.
+  4. Use the btest to check your functions for correctness.
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. 
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the btest to verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+/* 
+ * TMax - return maximum two's complement integer 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 4
+ *   Rating: 1
+ */
+int tmax(void) {
+  //(1 << 31) gives us the biggest possible negative number- by taking the complement, we get the biggest possible positive number
+  int x = ~(1 << 31);
+  return x;
+}
+/*
+ * isZero - returns 1 if x == 0, and 0 otherwise 
+ *   Examples: isZero(5) = 0, isZero(0) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int isZero(int x) {
+ //if x is zero, using bang will return 1, if x is not zero, using bang will return zero
+ return !x;
+}
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+  //the first int takes the complement of x&y, which gives us 1s in the spaces in which neither x or why has a zero
+  int xy_comp = ~(x&y);
+  //the following statement extracts the positions in which there is only a 1 in x or y
+  return ~(~(x&xy_comp)&~(y&xy_comp));
+}
+/* 
+ * isNotEqual - return 0 if x == y, and 1 otherwise 
+ *   Examples: isNotEqual(5,5) = 0, isNotEqual(4,5) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int isNotEqual(int x, int y) {
+  //the first int gives us the positions in which the numbers differ
+  int a = x^y;
+  //we use bang to convert this to a single bit and return it after banging it again
+  int b = !a;
+  return !b;
+}
+/* 
+ * sign - return 1 if positive, 0 if zero, and -1 if negative
+ *  Examples: sign(130) = 1
+ *            sign(-23) = -1
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 2
+ */
+int sign(int x) {
+   //the following line takes the negative of x, shifts it 31 bits to the right, then takes the negative of that-the code adds this to x shifted to the right by 31 bits
+   return  (x >> 31) + (~((~x+1) >> 31) + 1);
+}
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+  int a = !x;
+  int b = !a;
+  int c = ~b + 1;
+  return (c&y)|(~c&z);
+}
+/* 
+ * replaceByte(x,n,c) - Replace byte n in x with c
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: replaceByte(0x12345678,1,0xab) = 0x1234ab78
+ *   You can assume 0 <= n <= 3 and 0 <= c <= 255
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 3
+ */
+int replaceByte(int x, int n, int c) {
+  int a = c << (n << 3);
+  int b = x&~(0xff << (n << 3));
+  return a^b;
+}
+/* 
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
+ *            isAsciiDigit(0x05) = 0.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 3
+ */
+int isAsciiDigit(int x) {
+  //first, we check if the number is negative, then we check if it is between 0x30 and 0x39
+  int a  = ~(0x30);
+  int b = ~((1 << 31) | 0x39);
+  int c;
+  a = (1 << 31) & (a + x + 1) >> 31;
+  b = (1 << 31) & (b + x) >> 31;
+  c = !(a|b);
+  return c;
+}
+/* 
+ * subtractionOK - Determine if can compute x-y without overflow
+ *   Example: subtractionOK(0x80000000,0x80000000) = 1,
+ *            subtractionOK(0x80000000,0x70000000) = 0, 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+int subtractionOK(int x, int y) {
+  //this first int checks if only one of the numbers is negative
+  int a = (x >> 31)^(y >> 31);
+  //we try subtracting the two numbers- see what we get
+  int b = (x + (~y+1)) >> 31;
+  int c = a & (b^(x >> 31));
+  return !c;
+}
+// below are extra credits (4 pts in total)
+// 2 points each (1 correctness pt + 1 performance pt)
+/* 
+ * leastBitPos - return a mask that marks the position of the
+ *               least significant 1 bit. If x == 0, return 0
+ *   Example: leastBitPos(96) = 0x20
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ */
+int leastBitPos(int x) {
+  //by taking the complement of x, we switch the positions of the ones and zeros- when we add one, the 1 will carry until it lands in the spot in which there is only a 0, i.e. where there is a one in the original number
+  return ((~x + 1) & x);
+}
+/* 
+ * greatestBitPos - return a mask that marks the position of the
+ *               most significant 1 bit. If x == 0, return 0
+ *   Example: greatestBitPos(96) = 0x40
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 70
+ */
+int greatestBitPos(int x) {
+  // the following operations with x allow us to find the position of the first significatn 1 bit and turn the value of x into a number with zeros, followed by the first significant 1 bit, followed by 1s until the end, ex. 001010101 becomes 01111111
+  x = x | x>>1;
+  x = x | x>>2;
+  x = x | x>>4;
+  x = x | x>>8;
+  x = x | x>>16;
+  //the last operation extracts the position of the first significant 1 bit by using xor to get rid of all the other 1s besides the first significant. ex. 01111111 becomes 01000000
+  x = x & (((x)^(x >> 1)) | (1 << 31));
+  return x;
+}
